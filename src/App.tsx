ddd10ee5ac7bcea6fc141aa69ab4947812a52d72
Alfurquan Zahedi <alzahedi@microsoft.com>
2024-02-01 17:58:38 +0530

    Create a generic data fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { Grid, GridItem, Show } from '@chakra-ui/react'
 import './App.css'
 import Navbar from './components/Navbar'
 import GameGrid from './components/GameGrid'
+import GenreList from './components/GenreList'
 
 function App() {
 
   return (
     <Grid templateAreas={{
       base: `"nav" "main"`,
-      lg: `"nav nav" "main aside"`,
+      lg: `"nav nav" "aside main"`,
     }}>
       <GridItem area="nav"><Navbar /></GridItem>
       <Show above='lg'>
-      <GridItem area="aside">Aside</GridItem>
+      <GridItem area="aside"><GenreList /></GridItem>
       </Show>
       <GridItem area="main"><GameGrid /></GridItem>
     </Grid>
