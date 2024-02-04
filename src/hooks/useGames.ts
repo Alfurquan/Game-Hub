0953bd1b7def20289962c9a635212711aa71d5c3
Alfurquan Zahedi <alzahedi@microsoft.com>
2024-02-04 12:31:24 +0530

    Refactor: Create a reusable API client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import { FetchResponse } from "../services/api-client";
-import apiClient from "../services/api-client";
+import ApiClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
 export interface Game{
@@ -13,19 +12,20 @@ export interface Game{
     rating_top: number;
 }
 
+const apiClient = new ApiClient<Game>("/games");
+
 const useGames = (gameQuery: GameQuery) => 
     useQuery<FetchResponse<Game>, Error>({
         queryKey: ['games', gameQuery],
-        queryFn: () => 
-            apiClient.get<FetchResponse<Game>>('/games', {
+        queryFn: () =>
+            apiClient.getAll({
                 params : {
                     genres : gameQuery.genre?.id, 
                     platforms: gameQuery.platform?.id,
                     ordering: gameQuery.sortOrder,
                     search: gameQuery.searchText
                 }
-            })
-            .then(res => res.data),
+            })  
     })
 
                                                               
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client.ts";
-import { FetchResponse } from "../services/api-client";
+import ApiClient, { FetchResponse } from "../services/api-client";
 import genres from "../data/genres.ts";
 
 export interface Genre{
@@ -9,13 +8,12 @@ export interface Genre{
     image_background: string;
 }
 
+const apiClient = new ApiClient<Genre>("/genres");
  
 const useGenres = () => {
     return useQuery<FetchResponse<Genre>, Error>({
         queryKey: ['genres'],
-        queryFn: () =>
-            apiClient.get<FetchResponse<Genre>>('/genres')
-                     .then(res => res.data),
+        queryFn: apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000, // 24 hrs
         initialData: {count: genres.length, results: genres},
     }) 
