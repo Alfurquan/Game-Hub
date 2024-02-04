46410bfefbc8b247a0f89e0a133849c5dd3fde78
Alfurquan Zahedi <alzahedi@microsoft.com>
2024-02-04 12:21:06 +0530

    Refactor: Fetch games using react-query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 import apiClient from "../services/api-client";
 import platforms from "../data/platforms";
 
@@ -11,7 +11,7 @@ export interface Platform{
 
 const usePlatforms = () => useQuery<FetchResponse<Platform>, Error>({
     queryKey: ['platforms'],
-    queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms').then(res => res.data),
+    queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then(res => res.data),
     staleTime: 24 * 60 * 60 * 1000, // 24 hrs
     initialData: {count: platforms.length, results: platforms},
 })
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+    count: number;
+    results: T[];
+}
+
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params:{
         key: "52ec5c1c42e54663b7c9eeebb0402977"
     }
 })
+
